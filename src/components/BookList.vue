bcf2731f8603a72abe6c7f9d22ac992ea7e5cf4b
nottjp <43779227+nottjp@users.noreply.github.com>
2018-10-02 13:16:55 +0100

    Connecting Book Item

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,17 +2,22 @@
     <div>
         <h1>{{title}}</h1>
         <ul>
-            <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+            <book-item v-for:book="book"></book-item>
         </ul>
     </div>
 </template>
 
 <script>
+import BookItem from './BookItem';
+
 export default {
     name: 'BookList',
     data() {
         return {
             title: 'All Books',
+            components: {
+                BookItem
+            },
             books: [
                 {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
                 {title: 'American Gods', author: 'Neil Gaiman'},
