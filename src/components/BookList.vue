8906c034815435312f8fb6a0ad246e1fa6fb5364
nottjp <43779227+nottjp@users.noreply.github.com>
2018-10-02 13:18:19 +0100

    Connecting BookItem 2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>{{title}}</h1>
         <ul>
-            <book-item v-for:book="book"></book-item>
+            <book-item v-for:book="book in books"></book-item>
         </ul>
     </div>
 </template>
